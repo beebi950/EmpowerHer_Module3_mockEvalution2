dedfe5f5c468a726251c89a2baf057e5baa06bbb
beebi-k <kalagotlabeebi95@gmail.com>
2025-12-28 19:34:02 +0530

    App.jsx done

--- module3_evalution_mock2/src/App.jsx
+++ module3_evalution_mock2/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useState} from 'react';
+import { BrowserRouter ,Routes,Route,Navigate } from 'react-router-dom';
+import Login from "./pages/Login";
+import AdminDashboard from ".pages/AdminDashboard";
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [isAuth,setIsAuth]=useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return(
+    <BrowserRouter>
+
+      <Routes>
+        <Route path='/login' element={<Login setIsAuth={setIsAuth}/>}></Route>
+        <Route 
+        path="/admin" 
+        element={
+          <ProtectedRoute isAuth={isAuth}>
+            <AdminDashboard />
+          </ProtectedRoute>
+        
+        }>
+        </Route>
+
+        <Route path='*' element={<Navigate to='/login'/>}></Route>
+      </Routes>
+
+
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
